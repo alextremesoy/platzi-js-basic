1af67d11d83ce43ee684126572346e61e1ad6f1d
alextremesoy <soyalextreme@DESKTOP-ATPRFEE.localdomain>
2022-01-28 03:28:13 -0600

    [add] Notes for the introduction [add] Values [add] Variables

---
+++ code/values.js
@@ -0,0 +1,34 @@
+/* CLASS 3 
+    Values 
+*/
+
+
+// PRIMITIVE VALUES TYPES
+
+let number = 40; // Numbers
+let name = "Alejandro"; // string
+let trueValue = true; // boolean
+let falseValue = false;
+
+
+let nullish = null; // empty values -> placeholder values
+let myUndefined = undefined; 
+
+
+// OBJECT TYPE VALUES 
+let myArr = [1, 2, 3]; // array
+
+let person = {
+    name: "Alejandro",
+    age: 23
+};
+
+
+
+console.log(number);
+console.log(typeof(number));
+console.log(typeof(false));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(myArr));
+console.log(typeof(person))
---
+++ code/variables.js
@@ -0,0 +1,26 @@
+/*
+    CLASS 4
+    Variables
+ */
+
+
+// values to save on memory -> variables
+
+var namePerson = "Alejandro";
+
+
+// declare variable
+var age;
+
+// init the variable
+age = 23;
+
+// declare and init the var
+var countries = ["Mexico", "Colombia", "Argentina"];
+
+var person = {
+    namePerson,
+    age
+}
+
+person.namePerson;
